3d88c750146f302dc8e3c900615adce714473c90
Natalie-Rodriguez <natalie.rodriguez@betterment.com>
2019-10-10 17:45:07 -0400

    practice

---
+++ question1week1.rb
@@ -0,0 +1,14 @@
+# Read about the Ruby `each` method for arrays. Then refactor the code below using `each`.
+
+
+
+
+numbers = [1, 2, 4, 2]
+sum = 0
+index = 0
+numbers.length.each do 
+  number = numbers[index]
+  sum = sum + number
+  index = index + 1
+end 
+p sum
---
+++ things.rb
@@ -0,0 +1,6 @@
+DAYS_OF_THE_WEEK = %w(monday tuesday wednesday thursday friday)
+
+p DAYS_OF_THE_WEEK
+
+
+# How to Use attr_accessor, attr_writer & attr_reader
